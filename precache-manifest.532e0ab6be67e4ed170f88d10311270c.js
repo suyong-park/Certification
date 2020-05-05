8e33fe7e3cf5bda5ce104ede3f1a2286bb28ad2b
System Administrator <root@leeui-MacBookPro.local>
2020-05-06 00:22:33 +0900

    Updates

---
+++ precache-manifest.532e0ab6be67e4ed170f88d10311270c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c890b5a0748307501595df5e57914fd9",
+    "url": "/Certification/index.html"
+  },
+  {
+    "revision": "89ddbfd5df8c356c0f72",
+    "url": "/Certification/static/js/2.0584ceaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Certification/static/js/2.0584ceaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a087db73659782787316",
+    "url": "/Certification/static/js/main.4fd1cedf.chunk.js"
+  },
+  {
+    "revision": "84ec14c696a4172e179c",
+    "url": "/Certification/static/js/runtime-main.1ad5d51c.js"
+  }
+]);
